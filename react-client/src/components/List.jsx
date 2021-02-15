5530024e26ac78f5982da88ccedb2483d1db467f
Jacky Xia <jacky.w.xia@gmail.com>
2021-02-15 01:00:39 -0800

    Implement setTimeout to change after the length of the song, change displayed artists, routes for similarArtist and their top tracks

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ListItem from './ListItem.jsx';
 
-const List = (props) => (
-  <div>
-    <h4> List Component </h4>
-    There are { props.items.length } items.
-    { props.items.map(item => <ListItem item={item}/>)}
-  </div>
-)
+const List = ({artists}) => {
+  const artistNames = [];
+  artists.map((artist) => artistNames.push(artist.name))
+  return (
+    <div>Artists: {artistNames.join(', ')}</div>
+  );
+}
 
 export default List;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,33 +1,113 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Spotify from 'spotify-web-api-js';
 import List from './components/List.jsx';
+
+const spotifyWebApi = new Spotify();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    const params = this.getHashParams();
+
     this.state = {
-      items: []
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: {
+        name: 'Not Checked',
+        image: '',
+        isPlaying: false,
+        artists: [],
+        songLength: '',
+        similarArtists: [],
+        similarSongs: []
+      }
     }
+    if (params.access_token) {
+      spotifyWebApi.setAccessToken(params.access_token);
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items',
-      success: (data) => {
+    const { nowPlaying } = this.state;
+    const { isPlaying } = nowPlaying;
+    isPlaying ? null : this.getNowPlaying();
+  }
+
+  getNowPlaying() {
+    spotifyWebApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        console.log(response)
+        this.getRelatedArtists(response.item.artists[0].id);
         this.setState({
-          items: data
+          nowPlaying: {
+            name: response.item.name,
+            image: response.item.album.images[0].url,
+            isPlaying: response.is_playing,
+            artists: response.item.artists,
+            songLength: response.item.duration_ms
+          }
         })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  getRelatedArtists(artistId) {
+    spotifyWebApi.getArtistRelatedArtists(artistId)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          similarArtists: response.artists
+        });
+        this.getArtistTopTracks(response.artists[0].id)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  getArtistTopTracks(artistId) {
+    spotifyWebApi.getArtistTopTracks(artistId, 'US')
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          similarSongs: response.tracks
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+    const { nowPlaying } = this.state;
+    const { songLength } = nowPlaying;
+    console.log(songLength);
+    setTimeout(() => this.getNowPlaying(), songLength);
+    return (
+    <div>
+      <h1>MelodyMatch</h1>
+      <a href='http://localhost:8888'>
+        <button>Log in with Spotify</button>
+      </a>
+      <div>Now Playing: { this.state.nowPlaying.name }</div>
+      <div>
+        <img src ={this.state.nowPlaying.image} style={{width: 100}}/>
+      </div>
+      <List artists={this.state.nowPlaying.artists}/>
+      <button onClick={() => this.getNowPlaying()}>
+        Check Now Playing
+      </button>
     </div>)
   }
 }
